32de17dee450c6aee046e02176cfae45257f895e
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-05 09:22:22 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:digital_wallet/screens/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+  );
+}
